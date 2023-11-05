827e79ad7f352ba0377f3988d4cbe215f7279934
Aleksandr Lobanov <aleksandr.lobanov.official@gmail.com>
2023-11-05 20:38:58 +0500

    Update run.py

--- malevich_space/gql/task/run.py
+++ malevich_space/gql/task/run.py
@@ -3,7 +3,7 @@ from gql import gql
 
 run_task = gql(
     """
-    mutation RunCoreTask($task_id: String!, $raw: String, $org_id: String, $ca_override: [CAOverride]) {
+    mutation RunCoreTask($task_id: String!, $raw: String, $org_id: String, $ca_override: [CAOverride!]) {
       runWithStatus(
         taskId: $task_id,
         orgId: $org_id
