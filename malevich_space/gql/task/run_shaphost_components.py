b11f295dcc350f4eb273b760b20c8f209287027b
Aleksandr Lobanov <aleksandr.lobanov.official@gmail.com>
2023-12-05 18:05:45 +0300

    upd: renamed query

--- malevich_space/gql/task/run_shaphost_components.py
+++ malevich_space/gql/task/run_shaphost_components.py
@@ -3,7 +3,7 @@ from gql import gql
 
 get_run_snapshot_components = gql(
     """
-    query MyQuery($run_id: String!) {
+    query GetRunSnapshotComponents($run_id: String!) {
         run(uid: $run_id) {
             task {
             snapshot {
